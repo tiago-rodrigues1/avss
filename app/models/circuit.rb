904c91dfdc31684b0b9f4a43b8a5bb4f13cb56f5
Hadley <hadley.siqueira@gmail.com>
2023-10-03 21:08:14 -0300

    fix some bugs on deletion

--- app/models/circuit.rb
+++ app/models/circuit.rb
@@ -1,6 +1,6 @@
 class Circuit < ApplicationRecord
   belongs_to :user
-  has_many :circuit_stations
+  has_many :circuit_stations, dependent: :destroy
   has_many :stations, through: :circuit_stations
-  has_many :circuit_applications
+  has_many :circuit_applications, dependent: :destroy
 end
