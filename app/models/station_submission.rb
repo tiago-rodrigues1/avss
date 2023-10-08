4920e8a560776154af3bbbb596b6b896948565e8
Hadley <hadley.siqueira@gmail.com>
2023-10-08 15:46:57 -0300

    fix buf on circuit application destruction

--- app/models/station_submission.rb
+++ app/models/station_submission.rb
@@ -1,4 +1,7 @@
 class StationSubmission < ApplicationRecord
   belongs_to :circuit_submission
   belongs_to :station
+  has_many :objective_answers, dependent: :destroy
+  has_many :discursive_answers, dependent: :destroy
+  has_many :attachment_answers, dependent: :destroy
 end
