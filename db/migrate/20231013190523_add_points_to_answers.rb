930183d1a193bcd5daf4f14bc5ecba47976eaf23
Hadley <hadley.siqueira@gmail.com>
2023-10-13 18:06:15 -0300

    restore migration

--- db/migrate/20231013190523_add_points_to_answers.rb
+++ db/migrate/20231013190523_add_points_to_answers.rb
@@ -1,7 +1,7 @@
 class AddPointsToAnswers < ActiveRecord::Migration[7.0]
   def change
-    add_column :objective_answers, :points, :integer, null: false
-    add_column :discursive_answers, :points, :integer, null: false
-    add_column :attachment_answers, :points, :integer, null: false
+    add_column :objective_answers, :points, :integer
+    add_column :discursive_answers, :points, :integer
+    add_column :attachment_answers, :points, :integer
   end
 end
